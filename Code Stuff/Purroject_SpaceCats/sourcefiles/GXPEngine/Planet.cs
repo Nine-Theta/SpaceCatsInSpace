a4182ab2514beb0376452ed8bf398ffd1312808b
CHRISTIAN\Christian <433910@student.saxion.nl>
2017-02-06 14:52:12 +0100

    Added Ball and Planet classes

---
+++ Code Stuff/Purroject_SpaceCats/sourcefiles/GXPEngine/Planet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GXPEngine;
+
+namespace Purroject_SpaceCats
+{
+	public class Planet : Sprite
+	{
+		private Ball _hitball;
+		public Planet(Vec2 pPosVec) : base ("colors.png")
+		{
+			_hitball = new Ball(width / 2, pPosVec);
+		}
+	}
+}
